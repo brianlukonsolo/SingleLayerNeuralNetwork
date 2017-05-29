ca6a7f753ee72705f9b1485b7d3f9128f187d4aa
brianlukonsolo139 <brian.lukonsolo@gmail.com>
2017-05-29 20:26:08 +0100

    I have completed my second draft of the classes and made a proof-of-concept for the forward-pass portion of the network

--- src/com/brianlukonsolo/singlelayer_neural_network/NeuralNetwork/NeuralNetwork.java
+++ src/com/brianlukonsolo/singlelayer_neural_network/NeuralNetwork/NeuralNetwork.java
@@ -6,6 +6,7 @@ import com.brianlukonsolo.singlelayer_neural_network.NeuronTypes.InputNeuron;
 import com.brianlukonsolo.singlelayer_neural_network.NeuronTypes.OutputNeuron;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by lordmcbrian on 29/05/2017.
@@ -24,15 +25,57 @@ public class NeuralNetwork {
       input_layer.add(new InputNeuron(0.05));
       input_layer.add(new InputNeuron(0.10));
 
+      //-----ARRAYLIST OF THE INPUTS TO PASS TO THE HIDDEN LAYER NEURONS
+      ArrayList<Double> inputsForHiddenNeurons = new ArrayList<>();
+      for(InputNeuron inputNeuron: input_layer){
+          inputsForHiddenNeurons.add(inputNeuron.getInputValue());
+      }
       //add two hidden neurons to the hidden layer----------------------------------------HIDDEN NEURON LAYER
       ArrayList<HiddenNeuron> hidden_layer = new ArrayList<>();
-      hidden_layer.add(new HiddenNeuron());
-      hidden_layer.add(new HiddenNeuron());
+      hidden_layer.add(new HiddenNeuron(inputsForHiddenNeurons, 0.35));
+      hidden_layer.add(new HiddenNeuron(inputsForHiddenNeurons, 0.35));
+//Giving one of the hidden neurons some test weights
+
+      //====================TESTING ZONE=====================================================================########
+      //Hidden Neuron 1
+      HiddenNeuron test = new HiddenNeuron(inputsForHiddenNeurons, 0.35);
+      test.setWeightsOfInputs(new ArrayList<Double>(Arrays.asList(0.15,0.20))); //The weights of the synapses
+      double ans = test.calculateNetSum();
+      double output1 = test.calculateSigmoidOutput(ans);
+
+      //Hidden Neuron 2
+      HiddenNeuron test2 = new HiddenNeuron((inputsForHiddenNeurons), 0.35);
+      test2.setWeightsOfInputs(new ArrayList<>(Arrays.asList(0.25, 0.30)));
+      double ans2 = test2.calculateNetSum();
+      double output2 = test2.calculateSigmoidOutput(ans2);
+
+      //Add the neurons to hidden layer
+      hidden_layer.clear();
+      hidden_layer.add(test);
+      hidden_layer.add(test2);
+      //=====================END OF TESTING ZONE=============================================================########
+
+
+
+
+      //-----ARRAYLIST OF THE INPUTS TO PASS TO THE OUTPUT LAYER NEURONS
+      ArrayList<Double> inputsForOutputNeurons = new ArrayList<>();
+      for(HiddenNeuron hiddenNeuron: hidden_layer){
+          inputsForOutputNeurons.add(hiddenNeuron.getSigmoidOutputOfTheHiddenNeuron());
+      }
+
 
       //add two output neurons to the output layer----------------------------------------OUTPUT NEURON LAYER
       ArrayList<OutputNeuron> output_layer = new ArrayList<>();
-      output_layer.add(new OutputNeuron());
-      output_layer.add(new OutputNeuron());
+      OutputNeuron out1 = new OutputNeuron(inputsForOutputNeurons, 0.60);
+      out1.setWeightsOfOutputsFromHiddenLayer(new ArrayList<>(Arrays.asList(0.40, 0.45)));
+      double ns1 = out1.calculateNetSum();
+      double finalOut = out1.calculateSigmoidOutput(ns1);
+
+      OutputNeuron out2 = new OutputNeuron(inputsForOutputNeurons, 0.60);
+      out2.setWeightsOfOutputsFromHiddenLayer(new ArrayList(Arrays.asList(0.50, 0.55)));
+      double ns2 = out2.calculateNetSum();
+      double finalOut2 = out2.calculateSigmoidOutput(ns2);
 
 
       //Instatiate the full network and pass it the input neurons arrayList as inputs
--- src/com/brianlukonsolo/singlelayer_neural_network/NeuronTypes/OutputNeuron.java
+++ src/com/brianlukonsolo/singlelayer_neural_network/NeuronTypes/OutputNeuron.java
@@ -1,9 +1,127 @@
 package com.brianlukonsolo.singlelayer_neural_network.NeuronTypes;
 
+import java.util.ArrayList;
+
 /**
  * Created by lordmcbrian on 29/05/2017.
  */
+
+//Each output neuron only has backward connections in the network, from right to left
+// InputNeuron[O] ------ HiddenNeuron [o] -------- [[[o]]] <-- OutputNeuron
+
 public class OutputNeuron {
-    //Each output neuron only has backward connections in the network, from right to left
-    // InputNeuron[O] ------ HiddenNeuron [o] -------- [[[o]]] <-- OutputNeuron
+    //The hidden layer's outputs are used as inputs in the output layer
+    private ArrayList<Double> hiddenLayerOutputValuesList = new ArrayList<>();
+    //The weights of the synapses between the hidden layer outputs and the output layer
+    private ArrayList<Double> weightsOfOutputsFromHiddenLayer = new ArrayList<>();
+
+    //Target and actual outputs
+    private double targetOutput;
+    private double actualOutput;
+
+    //Error between target output and actual output
+    private double outputError;
+    private double bias;
+    //Storages: will be used during the back-propagation algorithm
+    private double sigmoidOutputOfTheOutputNeuron;
+    private double netSumOfTheInputsToTheOutputNeuron;
+
+
+    //Constructor
+    public OutputNeuron(ArrayList<Double> outputs_of_hidden_layer_list, double bias_neuron_value){
+        this.setHiddenLayerOutputValuesList(outputs_of_hidden_layer_list);
+        this.setBias(bias_neuron_value);
+    }
+
+    //Methods
+    public double calculateNetSum(){
+        //Net sum of the inputs to the neuron multiplied by their weights
+        double netSum = bias;
+        for(double d: hiddenLayerOutputValuesList) {
+            //For each input, multiply by it's weight in the weightsOfInputsList
+            netSum = netSum + (d * weightsOfOutputsFromHiddenLayer.get(hiddenLayerOutputValuesList.indexOf(d)));
+            //DEBUG
+            System.out.println("[ OutputNeuron Sum function ]>>> MULTIPLYING: " + d + " and " + weightsOfOutputsFromHiddenLayer.get(hiddenLayerOutputValuesList.indexOf(d)));
+            //Add to the total
+        }
+        //Store the value for later use
+        setNetSumOfTheInputsToTheOutputNeuron(netSum);
+
+        return netSum;
+    }
+
+    public double calculateSigmoidOutput(double netInput){
+        //Sigmoid activation function
+        double outputValue = (1/(1 + Math.exp(-netInput)));
+        //DEBUG
+        System.out.println("[ OutputNeuron Sigmoid function ]>>> Output of sigmoid: " + outputValue);
+        //Store the value for later use
+        setSigmoidOutputOfTheOutputNeuron(outputValue);
+        return outputValue;
+    }
+
+    //Get and Set
+    public double getTargetOutput() {
+        return targetOutput;
+    }
+
+    public void setTargetOutput(double targetOutput) {
+        this.targetOutput = targetOutput;
+    }
+
+    public double getActualOutput() {
+        return actualOutput;
+    }
+
+    public void setActualOutput(double actualOutput) {
+        this.actualOutput = actualOutput;
+    }
+
+    public double getOutputError() {
+        return outputError;
+    }
+
+    public void setOutputError(double outputError) {
+        this.outputError = outputError;
+    }
+
+    public double getBias() {
+        return bias;
+    }
+
+    public void setBias(double bias) {
+        this.bias = bias;
+    }
+
+    public ArrayList<Double> getHiddenLayerOutputValuesList() {
+        return hiddenLayerOutputValuesList;
+    }
+
+    public void setHiddenLayerOutputValuesList(ArrayList<Double> hiddenLayerOutputValuesList) {
+        this.hiddenLayerOutputValuesList = hiddenLayerOutputValuesList;
+    }
+
+    public ArrayList<Double> getWeightsOfOutputsFromHiddenLayer() {
+        return weightsOfOutputsFromHiddenLayer;
+    }
+
+    public void setWeightsOfOutputsFromHiddenLayer(ArrayList<Double> weightsOfOutputsFromHiddenLayer) {
+        this.weightsOfOutputsFromHiddenLayer = weightsOfOutputsFromHiddenLayer;
+    }
+
+    public double getSigmoidOutputOfTheOutputNeuron() {
+        return sigmoidOutputOfTheOutputNeuron;
+    }
+
+    public void setSigmoidOutputOfTheOutputNeuron(double sigmoidOutputOfTheOutputNeuron) {
+        this.sigmoidOutputOfTheOutputNeuron = sigmoidOutputOfTheOutputNeuron;
+    }
+
+    public double getNetSumOfTheInputsToTheOutputNeuron() {
+        return netSumOfTheInputsToTheOutputNeuron;
+    }
+
+    public void setNetSumOfTheInputsToTheOutputNeuron(double netSumOfTheInputsToTheOutputNeuron) {
+        this.netSumOfTheInputsToTheOutputNeuron = netSumOfTheInputsToTheOutputNeuron;
+    }
 }
